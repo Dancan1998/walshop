ad0b5b6b81b3a4f9aeff1775664ba69848419b7e
Dancan Chibole <dancankingstar@gmail.com>
2021-02-04 13:41:06 -0800

    User Registration and Password Encryption

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import express from "express";
 import connectDB from "./config/db.js";
 import colors from "colors";
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
@@ -15,7 +16,10 @@ app.get("/", (req, res) => {
   res.send("API is running");
 });
 
+app.use(express.json());
+
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 app.use(notFound);
 
