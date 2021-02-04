ea770f2fef544c41a021b0dc5c95de16d2722165
Dancan Chibole <dancankingstar@gmail.com>
2021-02-04 08:58:42 -0800

    Cart reducer and add to cart action

---
+++ frontend/src/reducers/cartReducers.js
@@ -0,0 +1,26 @@
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../constants/cartConstants";
+const initialState = {
+  cartItems: [],
+};
+
+export const cartReducer = (state = initialState, action) => {
+  if (action.type === CART_ADD_ITEM) {
+    const item = action.payload;
+    const existItem = state.cartItems.find((x) => x.product === item.product);
+
+    if (existItem) {
+      return {
+        ...state,
+        cartItems: state.cartItems.map((x) =>
+          x.product === existItem.product ? item : x
+        ),
+      };
+    } else {
+      return {
+        ...state,
+        cartItems: [...state.cartItems, item],
+      };
+    }
+  }
+  return state;
+};
