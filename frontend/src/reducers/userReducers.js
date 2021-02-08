d73fdea4aeffa66157e940c59460821cad1a48fe
Dancan Chibole <dancankingstar@gmail.com>
2021-02-08 13:30:49 -0800

    Register

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -3,6 +3,9 @@ import {
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCESS,
   USER_LOGOUT,
+  USER_REGISTER_FAIL,
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_SUCCESS,
 } from "../constants/userConstants";
 const initialState = {
   loading: false,
@@ -34,5 +37,30 @@ export const userLoginReducer = (state = initialState, action) => {
   if (action.type === USER_LOGOUT) {
     return {};
   }
+  return state;
+};
+
+export const userRegisterReducer = (state = initialState, action) => {
+  if (action.type === USER_REGISTER_REQUEST) {
+    return {
+      ...state,
+      loading: true,
+    };
+  }
+  if (action.type === USER_REGISTER_SUCCESS) {
+    return {
+      ...state,
+      loading: false,
+      userInfo: action.payload,
+    };
+  }
+  if (action.type === USER_REGISTER_FAIL) {
+    return {
+      ...state,
+      loading: false,
+      error: action.payload,
+    };
+  }
+
   return state;
 };
