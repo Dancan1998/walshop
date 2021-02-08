4ea94d102069334b6271f409ab67dab0e8a5fd74
Dancan Chibole <dancankingstar@gmail.com>
2021-02-08 12:00:41 -0800

    userLogin Reducer & Action

---
+++ frontend/src/reducers/userReducers.js
@@ -0,0 +1,37 @@
+import {
+  USER_LOGIN_FAIL,
+  USER_LOGIN_REQUEST,
+  USER_LOGIN_SUCCESS,
+  USER_LOGOUT,
+} from "../constants/userConstants";
+const initialState = {
+  loading: false,
+  userInfo: {},
+};
+
+export const userLoginReducer = (state = initialState, action) => {
+  if (action.type === USER_LOGIN_REQUEST) {
+    return {
+      ...state,
+      loading: true,
+    };
+  }
+  if (action.type === USER_LOGIN_SUCCESS) {
+    return {
+      ...state,
+      loading: false,
+      userInfo: action.payload,
+    };
+  }
+  if (action.type === USER_LOGIN_FAIL) {
+    return {
+      ...state,
+      loading: false,
+      error: action.payload,
+    };
+  }
+  if (action.type === USER_LOGOUT) {
+    return {};
+  }
+  return state;
+};
