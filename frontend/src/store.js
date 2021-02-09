b4d614c39587d693de101bf709e339c13f751a83
Dancan Chibole <dancankingstar@gmail.com>
2021-02-09 11:58:37 -0800

    PlaceOrder Screen and totals

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -35,10 +35,15 @@ const shippingAddressFromStorage = localStorage.getItem("shippingAddress")
   ? JSON.parse(localStorage.getItem("shippingAddress"))
   : {};
 
+const paymentMethodFromStorage = localStorage.getItem("paymentmethod")
+  ? JSON.parse(localStorage.getItem("paymentmethod"))
+  : "";
+
 const initialState = {
   cart: {
     cartItems: cartItemsFromStorage,
     shippingAddress: shippingAddressFromStorage,
+    paymentmethod: paymentMethodFromStorage,
   },
   userLogin: { userInfo: userInfoFromStorage },
 };
