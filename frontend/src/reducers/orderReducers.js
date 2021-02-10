db364719f8d9a9047f799503347eef45007aab78
Dancan Chibole <dancankingstar@gmail.com>
2021-02-10 11:46:36 -0800

    order pay reducer &  action

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -5,6 +5,10 @@ import {
   ORDER_DETAILS_REQUEST,
   ORDER_DETAILS_SUCCESS,
   ORDER_DETAILS_FAIL,
+  ORDER_PAY_REQUEST,
+  ORDER_PAY_SUCCESS,
+  ORDER_PAY_FAIL,
+  ORDER_PAY_RESET,
 } from "../constants/orderConstants";
 
 export const orderCreateReducer = (state = {}, action) => {
@@ -55,6 +59,33 @@ export const orderDetailsReducer = (
       loading: false,
       error: action.payload,
     };
+  }
+  return state;
+};
+
+export const orderPayReducer = (state = {}, action) => {
+  if (action.type === ORDER_PAY_REQUEST) {
+    return {
+      ...state,
+      loading: true,
+    };
+  }
+  if (action.type === ORDER_PAY_SUCCESS) {
+    return {
+      ...state,
+      loading: false,
+      success: true,
+    };
+  }
+  if (action.type === ORDER_PAY_FAIL) {
+    return {
+      ...state,
+      loading: false,
+      error: action.payload,
+    };
+  }
+  if (action.type === ORDER_PAY_RESET) {
+    return {};
   }
   return state;
 };
