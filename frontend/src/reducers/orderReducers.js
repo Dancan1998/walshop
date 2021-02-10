803650f896df716f688e6bea7d27c963c4c63cf0
Dancan Chibole <dancankingstar@gmail.com>
2021-02-10 09:40:57 -0800

    Order Details Reducer & Action

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -2,6 +2,9 @@ import {
   ORDER_CREATE_REQUEST,
   ORDER_CREATE_SUCCESS,
   ORDER_CREATE_FAIL,
+  ORDER_DETAILS_REQUEST,
+  ORDER_DETAILS_SUCCESS,
+  ORDER_DETAILS_FAIL,
 } from "../constants/orderConstants";
 
 export const orderCreateReducer = (state = {}, action) => {
@@ -20,6 +23,33 @@ export const orderCreateReducer = (state = {}, action) => {
     };
   }
   if (action.type === ORDER_CREATE_FAIL) {
+    return {
+      ...state,
+      loading: false,
+      error: action.payload,
+    };
+  }
+  return state;
+};
+
+export const orderDetailsReducer = (
+  state = { orderItems: [], shippingAddress: {} },
+  action
+) => {
+  if (action.type === ORDER_DETAILS_REQUEST) {
+    return {
+      ...state,
+      loading: true,
+    };
+  }
+  if (action.type === ORDER_DETAILS_SUCCESS) {
+    return {
+      ...state,
+      loading: false,
+      order: action.payload,
+    };
+  }
+  if (action.type === ORDER_DETAILS_FAIL) {
     return {
       ...state,
       loading: false,
